cefbf6601a4385f7c1fbbf98be772dc18993f2ca
Oleksandr Olashyn (dancingwithcrm) <aleksanderolashyn@gmail.com>
2022-08-17 23:35:26 -0400

    Updated list of common entities. Removed custom option

--- src/Helpers/EntityPicker.ts
+++ src/Helpers/EntityPicker.ts
@@ -6,40 +6,52 @@ interface EntityItem extends QuickPickItem {
 
 const commonEntities: EntityItem[] = [
   {
-    label: "Web Page",
-    value: "adx_webpage",
-  },
-  {
-    label: "Web Template",
-    value: "adx_webtemplate",
-  },
-  {
-    label: "Page Template",
-    value: "adx_pagetemplate",
-  },
-  {
-    label: "Web File",
-    value: "adx_webfile",
-  },
-  {
-    label: "Content Snippet",
-    value: "adx_contentsnippet",
-  },
-  {
-    label: "Table Permission",
-    value: "adx_entitypermission",
-  },
-  {
-    label: "Basic Form",
-    value: "adx_entityform",
-  },
-  {
-    label: "Advance Form",
+    label: "Advance Forms",
     value: "adx_webform",
   },
   {
-    label: "List",
+    label: "Basic Forms",
+    value: "adx_entityform",
+  },
+  {
+    label: "Content Snippets",
+    value: "adx_contentsnippet",
+  },
+  {
+    label: "Lists",
     value: "adx_entitylist",
+  },
+  {
+    label: "Page Templates",
+    value: "adx_pagetemplate",
+  },
+  {
+    label: "Poll Placements",
+    value: "adx_pollplacement",
+  },
+  {
+    label: "Polls",
+    value: "adx_poll",
+  },
+  {
+    label: "Table Permissions",
+    value: "adx_entitypermission",
+  },
+  {
+    label: "Web Files",
+    value: "adx_webfile",
+  },
+  {
+    label: "Web Pages",
+    value: "adx_webpage",
+  },
+  {
+    label: "Web Templates",
+    value: "adx_webtemplate",
+  },
+  {
+    label: "Weblink Sets",
+    value: "adx_weblinkset",
   },
 ];
 
@@ -53,16 +65,6 @@ export async function EntityPicker(placeHolder: string) {
       const selection = [...input.selectedItems];
       resolve(selection);
       input.hide();
-    });
-    input.onDidChangeValue(() => {
-      // add a new code to the pick list as the first item
-      if (input.value !== "" && input.items.findIndex((el) => el.value === input.value) === -1) {
-        const newItems = [
-          { label: input.value, value: input.value },
-          ...input.items,
-        ];
-        input.items = newItems;
-      }
     });
     input.onDidHide(() => input.dispose());
     input.show();
